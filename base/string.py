3a5925bb3fab7b3d0ff16a5775ff677b0da4e8de
张豪成 <zhanghaocheng@hhdd.com>
2018-11-13 21:04:06 +0800

    add->string 方法

---
+++ base/string.py
@@ -0,0 +1,55 @@
+# -*- coding: UTF-8 -*-
+def is_valid_mobile(str):
+    """
+    是否是有效的手机号
+    :param str
+    :return:
+    """
+
+
+def is_valid_email(str):
+    """
+    是否是有效的邮箱地址
+    :param str:
+    :return:
+    """
+
+
+def is_valid_id_num(str):
+    """
+    是否是有效的身份证号码
+    :param str:
+    :return:
+    """
+
+
+def get_all_mobiles(text):
+    """
+    获取text中所有的手机号
+    :param text:
+    :return:
+    """
+
+
+def get_all_emails(text):
+    """
+    获取text中所有的邮箱
+    :param text:
+    :return:
+    """
+
+
+def get_total_words(text):
+    """
+    获取text所有的单词
+    :param text:
+    :return:
+    """
+
+
+if __name__ == '__main__':
+    """
+    # 打开一个文本文件
+    # 读入其中的内容
+    # 分析文本的内容
+    """
