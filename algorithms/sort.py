c83f354018e2729db337ccc067023050ec0a5d1b
Randy <rainmonth@yeah.net>
2019-01-05 17:59:09 +0800

    add、rename,etc

---
+++ algorithms/sort.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# 作者： RandyZhang
+# 时间： 2019/1/5
+
+
+def bubblesort():
+    """
+    冒泡排序
+    :return:
+    """
+    pass
+
+
+def cocktailshakersort():
+    """
+    双向冒泡排序
+    :return:
+    """
+
+
+def insertionsort():
+    """
+    插入排序
+    :return:
+    """
+    pass
+
+
+def mergesort():
+    """
+    归并排序
+    :return:
+    """
+    pass
+
+
+def quictsort():
+    """
+    快速排序
+    :return:
+    """
+    pass
+
+
+def selectsort():
+    """
+    选择排序
+    :return:
+    """
+    pass
+
+
+def shellsort():
+    """
+    希尔排序
+    :return:
+    """
+    pass
+
+
+def bucketsort():
+    """
+    桶排序
+    :return:
+    """
+
+
+if __name__ == '__main__':
+    pass
--- spider/SpiderDouBanMusic.py
+++ spider/SpiderDouBanMusic.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+# 描述： 豆瓣音乐爬虫
 # 作者： RandyZhang
 # 时间： 2018/11/9
 
