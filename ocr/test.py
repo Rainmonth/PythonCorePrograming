7467555eb4e4178847d4bd7a2616653063cfb31c
张豪成 <zhanghaocheng@hhdd.com>
2019-05-31 13:17:49 +0800

    orc

--- ocr/test.py
+++ ocr/test.py
@@ -5,8 +5,10 @@ from PIL import Image
 # im = im.convert('L')
 # im.save("result.jpg")
 path2 = "result.jpg"
-path3 = "test3.jpg"
+path3 = "sss.jpg"
 im2 = Image.open(path3)
+im2 = im2.convert("L")
+im2.save("sss_bw.jpg")
 
 print(pytesseract.image_to_string(im2.convert("L"), lang='chi_sim'))
 # pytesseract.pytesseract.tesseract_cmd = 'C://Program Files (x86)/Tesseract-OCR/tesseract.exe'
