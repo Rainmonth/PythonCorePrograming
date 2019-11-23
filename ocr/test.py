da7a5f3f20a9956436ae0a0fcc9cddd55df975ee
张豪成 <zhanghaocheng@hhdd.com>
2019-11-23 14:57:29 +0800

    mod->增加ocr测试

--- ocr/test.py
+++ ocr/test.py
@@ -1,14 +1,14 @@
 import pytesseract
 from PIL import Image
-# path = "test.jpg"
+path = "test.jpg"
 # im = Image.open(path)
 # im = im.convert('L')
 # im.save("result.jpg")
-path2 = "result.jpg"
-path3 = "sss.jpg"
-im2 = Image.open(path3)
-im2 = im2.convert("L")
-im2.save("sss_bw.jpg")
+# path2 = "result.jpg"
+# path3 = "sss.jpg"
+im2 = Image.open(path)
+# im2 = im2.convert("L")
+# im2.save("sss_bw.jpg")
 
 print(pytesseract.image_to_string(im2.convert("L"), lang='chi_sim'))
 # pytesseract.pytesseract.tesseract_cmd = 'C://Program Files (x86)/Tesseract-OCR/tesseract.exe'
